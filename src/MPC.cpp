abe4b40d18ccad86507f4f9315ca4599cc47c881
nickpoerio <nickpoerio@hotmail.com>
2018-07-04 23:23:07 +0200

    #4b

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -94,8 +94,8 @@ class FG_eval {
 		AD<double> delta0 = vars[delta_start + t - 1];
 		AD<double> a0 = vars[a_start + t - 1];
 		
-		AD<double> f0 = coeffs[0]+coeffs[1]*x0+coeffs[2]*pow(x0,2)+coeffs[3]*pow(x0,3);
-		AD<double> psides0 = CppAD::atan(coeffs[1]+2*coeffs[2]*x0+3*coeffs[3]*pow(x0,2));
+		AD<double> f0 = coeffs[0]+coeffs[1]*x0+coeffs[2]*CppAD::pow(x0,2)+coeffs[3]*CppAD::pow(x0,3);
+		AD<double> psides0 = CppAD::atan(coeffs[1]+2*coeffs[2]*x0+3*coeffs[3]*CppAD::pow(x0,2));
 		
 		// remaining constraints
 		fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
