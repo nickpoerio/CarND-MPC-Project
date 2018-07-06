a8ffbf9ba7fae29caff7b0472a5a2bc77292f5c7
nickpoerio <nickpoerio@hotmail.com>
2018-07-07 00:03:13 +0200

    #7f

--- src/main.cpp
+++ src/main.cpp
@@ -162,11 +162,10 @@ int main() {
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-		  /*for (unsigned int i=0;i<ptsx_loc.size();i++){
-			  mpc_x_vals[i]=ptsx_loc[i];
-			  mpc_y_vals[i]=ptsy_loc[i];
+		  for (unsigned int i=0;i<ptsx_loc.size();i++){
+			  mpc_x_vals.push_back(ptsx_loc[i]);
+			  mpc_y_vals.push_back(ptsy_loc[i]);
 		  }
-		  */
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
