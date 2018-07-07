22529a70ce547ff4aa94da4cc95f7d5aaf60bcc0
nickpoerio <nickpoerio@hotmail.com>
2018-07-08 01:34:41 +0200

    tune2

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -49,13 +49,13 @@ class FG_eval {
     // Cost function
     for (unsigned int t = 0; t < N; t++) {
       fg[0] += 1.*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 100.*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++) {
-      fg[0] += 1000.*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 10.*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
 
