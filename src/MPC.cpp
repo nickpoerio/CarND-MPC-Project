1d803647dc33f5fde1f4dff216003173b6a3698f
nickpoerio <nickpoerio@hotmail.com>
2018-07-09 00:09:30 +0200

    tune10c

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -48,8 +48,8 @@ class FG_eval {
 
     // Cost function
     for (unsigned int t = 0; t < N; t++) {
-      fg[0] += 5.*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 5000.*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += 1.*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
