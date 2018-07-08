bec6396e7d9a3c08e9852eeb8997ada56e3f8409
nickpoerio <nickpoerio@hotmail.com>
2018-07-09 01:21:04 +0200

    tune 2.7g 1e4 dthrottle/brake

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -43,7 +43,7 @@ class FG_eval {
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
-	double ref_v = CppAD::sqrt(30./(CppAD::abs(coeffs[2])+1e-5));
+	double ref_v = CppAD::sqrt(27./(CppAD::abs(coeffs[2])+1e-5));
 
 	fg[0] = 0;
 
@@ -57,14 +57,14 @@ class FG_eval {
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++) {
       fg[0] += 10.*CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 10.*CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 1.*CppAD::pow(vars[a_start + t], 2);
 	  
     }
 
     // Minimize the value gap between sequential actuations.
     for (unsigned int t = 0; t < N - 2; t++) {
       fg[0] += 1.*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += 1000.*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 10000.*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 	
 	// initial constraints
