fa0641c42bf1989bbcbfeb5808c884a66d4484b7
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 01:16:50 +0200

    sol5

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -51,12 +51,13 @@ class FG_eval {
       fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
 	  
-	  double yp = coeffs[1]+2*coeffs[2]*t+3*coeffs[3]*CppAD::pow(t,2);
-	  double ypp = 2*coeffs[2]+6*coeffs[3]*t;
+	  AD<double> x = vars[x_start + t];
+	  AD<double> yp = coeffs[1]+2*coeffs[2]*x+3*coeffs[3]*CppAD::pow(x,2);
+	  AD<double> ypp = 2*coeffs[2]+6*coeffs[3]*x;
 	  
-	  double curv = CppAD::abs(ypp)/CppAD::pow(1+CppAD::pow(yp,2),1.5);
+	  AD<double> curv = CppAD::abs(ypp)/CppAD::pow(1+CppAD::pow(yp,2),1.5);
 	  
-	  double ref_v = CppAD::sqrt(55/(curv+1e-5));
+	  AD<double> ref_v = CppAD::sqrt(55/(curv+1e-5));
       fg[0] += 1.*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
