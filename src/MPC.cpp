4c6d1c9c7308b51a94d257628ebbbf05e71eae56
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 01:40:09 +0200

    sol6c

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -52,7 +52,7 @@ class FG_eval {
       fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
 	  
-	  double x = x + CppAD::Value(vars[v_start + t])*0.1;
+	  double x = x + vars[v_start + t]*0.1;
 	  double yp = coeffs[1]+2*coeffs[2]*x+3*coeffs[3]*pow(x,2);
 	  double ypp = 2*coeffs[2]+6*coeffs[3]*x;
 	  
