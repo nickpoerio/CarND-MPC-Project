8c6417c9075dca75247982e5136a82d84cc004d0
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 01:34:13 +0200

    sol6b

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -46,13 +46,13 @@ class FG_eval {
     // the Solver function below.
 
 	fg[0] = 0;
-
+	double x = 0;
     // Cost function
     for (unsigned int t = 0; t < N; t++) {
       fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
 	  
-	  double x = CppAD::Value(vars[x_start + t]);
+	  double x += CppAD::Value(vars[v_start + t])*0.1;
 	  double yp = coeffs[1]+2*coeffs[2]*x+3*coeffs[3]*pow(x,2);
 	  double ypp = 2*coeffs[2]+6*coeffs[3]*x;
 	  
