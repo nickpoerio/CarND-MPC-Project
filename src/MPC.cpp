09e49dc52e229b4da01b68020d1940c4f38b96e2
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 19:46:01 +0200

    sol9

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -59,7 +59,7 @@ class FG_eval {
 	  
 	  AD<double> curv = abs(ypp)/pow(1+pow(yp,2),1.5);
 	  
-	  AD<double> ref_v = CppAD::fmin(110,sqrt(200/(curv+1e-5))); //aumentare ancora
+	  AD<double> ref_v = fmin(110.,sqrt(200/(curv+1e-5))); //aumentare ancora
       fg[0] += 1.*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
