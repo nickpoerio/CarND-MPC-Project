099926d33c5a14fef9c9dc0170837d33641dbe79
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 19:36:25 +0200

    sol9

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -46,19 +46,20 @@ class FG_eval {
     // the Solver function below.
 
 	fg[0] = 0;
-	AD<double> x = vars[v_start]*0;
+	AD<double> x = 0;
+	AD<double> yp = 0;
     // Cost function
     for (unsigned int t = 0; t < N; t++) {
       fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 1000.*CppAD::pow(vars[epsi_start + t], 2);
 	  
-	  AD<double> x = x + vars[v_start + t]*dt;
+	  AD<double> x = x + vars[v_start + t]*cos(atan(yp))*dt;
 	  AD<double> yp = coeffs[1]+2*coeffs[2]*x+3*coeffs[3]*pow(x,2);
 	  AD<double> ypp = 2*coeffs[2]+6*coeffs[3]*x;
 	  
 	  AD<double> curv = abs(ypp)/pow(1+pow(yp,2),1.5);
 	  
-	  AD<double> ref_v = sqrt(200/(curv+1e-5));
+	  AD<double> ref_v = min(110,sqrt(200/(curv+1e-5))); //aumentare ancora
       fg[0] += 1.*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
