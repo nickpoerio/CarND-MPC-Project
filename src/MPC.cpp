e7698beb7408a03b89437f1b4fc5d763fa9d8552
nickpoerio <nickpoerio@hotmail.com>
2018-07-11 20:14:26 +0200

    sol11

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -48,7 +48,6 @@ class FG_eval {
 	fg[0] = 0;
 	AD<double> x = 0;
 	AD<double> yp = 0;
-	AD<double> vmax = 110;
     // Cost function
     for (unsigned int t = 0; t < N; t++) {
       fg[0] += 10.*CppAD::pow(vars[cte_start + t], 2);
@@ -60,7 +59,7 @@ class FG_eval {
 	  
 	  AD<double> curv = abs(ypp)/pow(1+pow(yp,2),1.5);
 	  
-	  AD<double> ref_v = sqrt(170/(curv+1e-2)); //aumentare ancora
+	  AD<double> ref_v = sqrt(150/(curv+1e-3)); //aumentare ancora
       fg[0] += 1.*CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
